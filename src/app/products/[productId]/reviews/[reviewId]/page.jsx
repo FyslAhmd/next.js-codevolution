aade51fdc0c84cc2f3927eae1aef4eed3659782e
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-19 16:37:31 +0600

    make custom not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.jsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 const ReviewPage = async ({ params }) => {
   const { reviewId } = await params;
+  if (reviewId > 1000) {
+    notFound();
+  }
   return (
     <div>
       <h1>This is review {reviewId}</h1>
