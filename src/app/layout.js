9e3b7bbc7c3de5d3ab35339d28af74ccdbf9e0df
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-19 17:35:24 +0600

    concept of layout and nested layout is clear

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,25 @@
-export default function RootLayout({children}) {
-    return (
-        <html lang="en">
-            <body>
-                <main>
-                    {children}
-                </main>
-            </body>
-        </html>
-    );
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+          }}
+        >
+          <p>Header</p>
+        </header>
+        {children}
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}
+        >
+          <p>Footer</p>
+        </footer>
+      </body>
+    </html>
+  );
 }
---
+++ src/app/products/[productId]/layout.jsx
@@ -0,0 +1,8 @@
+export default function ProductLayout({children}) {
+    return (
+        <section>
+            {children}
+            <h2>Featured Products</h2>
+        </section>
+    );
+}
