46d9ec6a1ba4da8c10830c37e1495bbc87944e1b
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-22 01:46:23 +0600

    learn all about metadata title

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
+
+export const metadata = {
+  title: "Blog",
+};
 
 const Blog = () => {
-    return (
-        <div>
-            This is Blog Page
-        </div>
-    );
+  return <div>This is Blog Page</div>;
 };
 
 export default Blog;
--- src/app/blog/second/page.jsx
+++ src/app/blog/second/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+export const metadata = {
+  title: {
+    absolute: "Second Blog",
+  },
+};
 
 const SecondBlog = () => {
-    return (
-        <div>
-            This is second blog
-        </div>
-    );
+  return <div>This is second blog</div>;
 };
 
 export default SecondBlog;
