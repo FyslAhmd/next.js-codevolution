3322e94854d283ffbbf82f40b8f98cedf9449b29
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-23 01:13:25 +0600

    handle error using error page

--- src/app/products/[productId]/reviews/[reviewId]/page.jsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.jsx
@@ -1,8 +1,16 @@
 import { redirect } from "next/navigation";
 import React from "react";
 
+const getRandomInt = (count) => {
+  return Math.floor(Math.random() * count);
+};
+
 const ReviewPage = async ({ params }) => {
   const { reviewId } = await params;
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
   if (reviewId > 1000) {
     redirect("/products");
   }
