ac527f371693ead369a32950b2fb4f8f9596cf17
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-23 00:56:12 +0600

    known the use of template file

--- src/app/(auth)/template.jsx
+++ src/app/(auth)/template.jsx
@@ -1,16 +1,26 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
   { name: "Login", href: "/login" },
   { name: "Forgot Password", href: "/forgot-password" },
 ];
-export default function Layout({ children }) {
+export default function AuthLayout({ children }) {
   const pathname = usePathname();
+  const [input, setInput] = useState("");
   return (
     <section>
+      <div>
+        <input
+          type="text"
+          className="border m-2 p-2 w-2/6"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
       {navLinks.map((link, ind) => {
         const isActive =
           pathname === link.href ||
